d6f24d5acf63d77a44a290834d5a1e01c527ef30
Eric Li <ericut@gmail.com>
2021-08-11 11:28:46 -0300

    page: placar data inicial corrigida para teste

--- src/context/placar/data/placaresData.ts
+++ src/context/placar/data/placaresData.ts
@@ -3,22 +3,12 @@ import { IPlacaresProps } from '../../../interfaces/placar';
 export const placaresData: IPlacaresProps[] = [
   {
     id_placar: 'any-id',
-    nome: 'Jogatina Coup (Exemplo)',
+    nome: 'Placar Coup (Exemplo)',
     jogo: 'Coup',
-    jogadores: ['Mr Li', 'Iranilo', 'Manin'],
+    jogadores: ['Jogador', 'Coelho', 'Falapovo'],
     status: 'Ativo',
-    partidas: 2,
-    data_inicio: '2021-07-29',
-    data_fim: '',
-  },
-  {
-    id_placar: 'outro-any-id',
-    nome: 'Jogatina Saboteur (Exemplo)',
-    jogo: 'Saboteur',
-    jogadores: ['Mr Li', 'Iranilo', 'Manin'],
-    status: 'Finalizado',
     partidas: 1,
-    data_inicio: '2021-07-29',
+    data_inicio: '2021-08-10',
     data_fim: '',
   },
 ];
